90ae778bd8b61b1cbe741cbbba034fcccc32dbda
mozuki12 <84673534+mozuki12@users.noreply.github.com>
2022-11-27 20:36:35 -0600

    first commit

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    nama: {
+        type: String,
+        required: [true, 'Silahkan isi nama'],
+        unique: true
+    }, 
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [ /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Silahkan isi email valid!']
+    }
+})
+
+module.exports = mongoose.model('User', UserSchema)
